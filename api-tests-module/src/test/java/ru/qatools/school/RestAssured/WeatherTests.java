a83936e4efef2c5cd8c4eac669f9d013b449b6b5
alekspog <aleksey.pogibelev@gmail.com>
2016-05-08 23:41:53 +0300

    add test for city name suggest

--- api-tests-module/src/test/java/ru/qatools/school/RestAssured/WeatherTests.java
+++ api-tests-module/src/test/java/ru/qatools/school/RestAssured/WeatherTests.java
@@ -5,11 +5,15 @@ import org.junit.Test;
 import ru.yandex.qatools.allure.annotations.Title;
 
 import static com.jayway.restassured.RestAssured.given;
+import static org.hamcrest.core.IsEqual.equalTo;
 
 /**
  * Created by alekspog on 07.05.2016.
  */
 public class WeatherTests {
+
+    public static final String PortLouis = "Port-Louis";
+
 
     @Test
     @Title("Запрос к api/cities возврашает 200")
@@ -18,6 +22,15 @@ public class WeatherTests {
                 .basePath("api").param("limit", 5)
                 .get("cities")
                 .then().assertThat().statusCode(HttpStatus.SC_OK);
+    }
+
+    @Test
+    @Title("Запрос к api/suggest с названием города возврашает список с искомым городом")
+    public void ShouldReturnCityNameInSuggest() throws Exception{
+        given().baseUri("http://weather.lanwen.ru")
+                .basePath("api").param("query", PortLouis)
+                .get("suggest")
+                .then().assertThat().body("[0].name", equalTo(PortLouis));
     }
 
 }
